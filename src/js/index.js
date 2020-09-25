ff956e52d7b2cfe7a2cfda01f6741685b262510f
Bartosz Malecki <bartoszmalecki90@gmail.com>
2020-09-25 15:00:34 +0200

    save storage done

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,20 @@ import '../scss/main.scss';
 // registerSW();
 
 /* place your code below */
+
+
+const entry = localStorage.getItem('entry');
+let result = '';
+
+if (entry) {
+    result = entry;
+}
+
+const entryTextarea = document.querySelector('.text--js');
+entryTextarea.value = result;
+
+const buttonSave = document.querySelector('.buttons_button--save--js');
+
+buttonSave.addEventListener('click', () => {
+    localStorage.setItem('entry', entryTextarea.value);
+})
